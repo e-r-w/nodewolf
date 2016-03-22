1ece13384783cf45cfd1c943f533d152904631b0
e-r-w <ethan.r.williams@live.com>
2016-03-22 12:42:59 +1100

    remove slackbot_id from server stuff

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('../core')
-  .run(process.env.SLACK_TOKEN, process.env.SLACK_CHANNEL, process.env.SLACKKBOT_ID)
+  .run(process.env.SLACK_TOKEN, process.env.SLACK_CHANNEL)
   .then(
     () => console.log('Game running!'),
     err => console.log(`bot failed to start:\n${JSON.stringify(err)}`)
